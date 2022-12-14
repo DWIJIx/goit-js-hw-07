a9705f47f44a975a418e4ac4bac1415356b12282
DWIJIx <106489060+DWIJIx@users.noreply.github.com>
2022-12-14 11:21:10 +0200

    made markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems[0].original);
+
+
+
+/* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div> */
+
+const imgMarkup = galleryItem =>
+`
+<div class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="${galleryItem.original}"
+      alt="${galleryItem.description}"
+    />
+  </a>
+</div>
+`
+// console.log(imgMarkup(galleryItems[0]))
+
+const imagesMarkup = galleryItems.map(imgMarkup).join('')
+
+console.log(imagesMarkup)
+
+const divContiner = document.querySelector('.gallery')
+
+divContiner.insertAdjacentHTML('afterbegin', imagesMarkup)
