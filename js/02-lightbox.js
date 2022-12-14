2456d1174821add683b1fc9d20778783b75a8346
DWIJIx <106489060+DWIJIx@users.noreply.github.com>
2022-12-14 22:10:34 +0200

    made task 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const ulContiner = document.querySelector('.gallery')
+
+const imgMarkup = galleryItem => 
+    `<a class="gallery__item" href="${galleryItem.original}">
+        <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}"/>
+    </a>
+    `
+const imagesMarkup = galleryItems.map(imgMarkup).join('')
+// console.log(imagesMarkup)
+
+ulContiner.insertAdjacentHTML('beforeend', imagesMarkup)
+
+ulContiner.addEventListener('click', onUlContainerClick)
+
+function onUlContainerClick(evt) {
+    evt.preventDefault()
+
+    if (!evt.target.classList.contains('gallery__image')) {
+      return;
+  }
+
+    var lightbox = new SimpleLightbox('.gallery a',
+        { 
+            showCounter: false,
+            captionsData: 'alt',
+            captionDelay: 250
+    });
+}
+
+
